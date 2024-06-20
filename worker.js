79667ead5438499c273f72f56c74e55b0fc8e7f8
Jeroen Vaelen <jeroen.vaelen@happening.xyz>
2024-06-20 15:24:24 +0200

    show input every 5s in the middle of the tab

--- worker.js
+++ worker.js
@@ -1,19 +1,36 @@
 let popupWindowId = null;
 
-setInterval(() => {
-  if (!popupWindowId) {
-    chrome.windows.create({
-      url: 'popup.html',
-      type: 'popup',
-      width: 300,
-      height: 150,
-      left: left,
-      top: top,
-    }, (window) => {
-      popupWindowId = window.id;
-    });
+async function getScreenSizeAndCreatePopup() {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  if (tab) {
+    createPopup(tab.width, tab.height);
   }
-}, 10000);
+}
+
+function createPopup(screenWidth, screenHeight) {
+  const popupWidth = 400;
+  const popupHeight = 150;
+  const left = Math.round((screenWidth - popupWidth) / 2);
+  const top = Math.round((screenHeight - popupHeight) / 2);
+
+  if (!popupWindowId) {
+    chrome.windows.create(
+      {
+        url: 'popup.html',
+        type: 'popup',
+        width: popupWidth,
+        height: popupHeight,
+        left: left,
+        top: top,
+      },
+      (window) => {
+        popupWindowId = window.id;
+      }
+    );
+  }
+}
+
+setInterval(() => getScreenSizeAndCreatePopup(), 5000);
 
 chrome.windows.onRemoved.addListener((windowId) => {
   if (windowId === popupWindowId) {
